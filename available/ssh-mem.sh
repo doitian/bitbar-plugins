71ad84b619dabba7cb171b313e6ad9d2b0bcec74
ian <me@iany.me>
2017-03-02 18:13:22 +0800

    update script

--- available/ssh-mem.sh
+++ available/ssh-mem.sh
@@ -13,6 +13,10 @@ fi
 if (( curmem > minmem )); then
   echo "${curmem_label} | color=green"
 else
+  answer=$(terminal-notifier -title "Memory Alert" -message "$host free memory $curmem_label, ssh to it?" -actions Yes -closeLabel No -timeout 30)
+  if [ "$answer" = "Yes" ]; then
+    osascript -e 'tell application "Terminal" to do script "'"ssh $host"'"'
+  fi
   echo "${curmem_label} | color=red"
 fi
 echo "----"
